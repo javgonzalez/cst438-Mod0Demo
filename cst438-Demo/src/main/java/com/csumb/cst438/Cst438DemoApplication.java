2d46b473fffee6b13c50dcb92513ea7c90c529cb
javgonzalez <javgonzalez@csumb.edu>
2022-04-26 12:22:33 -0700

    First Commit

---
+++ cst438-Demo/src/main/java/com/csumb/cst438/Cst438DemoApplication.java
@@ -0,0 +1,14 @@
+package com.csumb.cst438;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Cst438DemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Cst438DemoApplication.class, args);
+		System.out.println("First Commit");
+	}
+
+}
---
+++ cst438-Demo/src/test/java/com/csumb/cst438/Cst438DemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.csumb.cst438;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Cst438DemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
